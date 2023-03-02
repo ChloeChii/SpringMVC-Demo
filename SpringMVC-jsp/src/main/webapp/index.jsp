046f249958915c918a580acadded7f314ffefda9
chloe <chloe4wk@gmail.com>
2023-03-01 21:29:16 -0800

    Configure and use InternalResourceView and RedirectView

---
+++ SpringMVC-jsp/src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<%@ page contentType="text/html; charset=UTF-8" language="java" %>
+<html>
+<body>
+<h1>首頁</h1>
+<a href="${pageContext.request.contextPath}/success">success.jsp</a>
+</body>
+</html>
--- SpringMVC_Demo3/src/main/java/com/atyochenzen/mvc/controller/TestController.java
+++ SpringMVC_Demo3/src/main/java/com/atyochenzen/mvc/controller/TestController.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
-    @RequestMapping("/")
-    public String index(){
-        return "index";
+//    @RequestMapping("/")
+//    public String index(){
+//        return "index";
+//    }
+
+    @RequestMapping("/test_view")
+    public String testView(){
+        return "test_view";
     }
 }
